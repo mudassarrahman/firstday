87f8729a526acdbfa3c89173ae4eef6b980aa4c9
mudassarrahman <>
2021-04-28 07:39:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+age = input("enter you are current age: ")
+
+print("for example if you would live upto 60 years then your ")
+
+entire_age = 60
+
+entire_weeks = 60 * 52
+
+entire_days = 60 * 52 * 365
+
+
+print(f"the time you have left in years is {entire_age - int(age)}\n and the time you have left in weeks is {entire_weeks - int(age) * 52}\n and the time you have left in days is {entire_days - int(age) * 52 * 365}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
